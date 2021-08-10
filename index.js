981a92079935f3709602b5d14651b50900e70d16
Tanisha-creator <tanishakindo@gmail.com>
2021-08-10 22:21:31 +0530

    Card Function added

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const taskContainer=document.querySelector(".task__container");
+
+const generateNewCard = (taskData) =>
+    `
+    <div class="col-md-6 col-lg-4" id=${taskData.id}>
+      <div class="card">
+        <div class="card-header d-flex justify-content-end gap-2">
+            <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+            <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <img src=${taskData.imageUrl} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${taskData.taskTitle}</h5>
+          <p class="card-text">${taskData.taskType}</p>
+          <a href="#" class="btn btn-dark float-start">${taskData.taskDescription}</a>
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-outline-primary float-end"> Open Task </button>
+        </div>
+      </div>
+    </div>
+    `;
+
 const saveChanges = () => {
     const taskData= {
         id: `${Date.now()}`,   //Unique Number for id
@@ -5,10 +28,12 @@ const saveChanges = () => {
         taskTitle: document.getElementById("tasktitle").value,
         taskType: document.getElementById("tasktype").value,
         taskDescription: document.getElementById("taskdescription").value,
+     }; 
+
+     taskContainer.insertAdjacentHTML("beforeend", generateNewCard (taskData));
      };
-     
-     console.log(taskData);
-    };
+
+    
     
 
     
